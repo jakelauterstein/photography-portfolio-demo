2a0fd05d28e05cc24520a2d5affbfa96d69d62d8
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-06-24 18:20:54 -0500

    added Navbar

---
+++ src/components/Nav/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
+
+function Nav() {
+
+    const categories = [
+        {
+            name: "commerical",
+            description:
+            "Photos of grocery stores, food trucks, and other commercial projects"
+        },
+        {
+            name: "portraits",
+            description:
+            "Portraits of people in my life"
+        },
+        {
+            name: "landscape",
+            description:
+            "Fields, farmhouses, waterfalls, and the beauty of nature"
+        }
+    ]
+
+    const handleClick = () => {
+        console.log("click handled")
+      }
+
+    return (
+    <header>
+        <h2>
+            <a href="/">
+                <span role="img" aria-label="camera">📸</span> Oh Snap!
+            </a>
+        </h2>
+        <nav>
+            <ul className="flex-row">
+                <li className="mx-2">
+                <a href="#about" onClick={() => handleClick()}>                        
+                About me
+                    </a>                
+                </li>
+                <li>
+                    <span>Contact</span>
+                </li>
+                {categories.map((category) => (
+                    <li className="mx-1" key={category.name}>
+                        <span onClick={() => { handleClick(); }}>
+                        {capitalizeFirstLetter(category.name)}
+                        </span>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    </header>
+    );
+};
+
+export default Nav;
